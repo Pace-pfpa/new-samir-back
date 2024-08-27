fff5450d938a5abc78ab29218e9555ea563be3a6
Luan Henrique <luanhsantos14@gmail.com>
2024-08-27 13:01:05 -0300

    first commit

---
+++ src/main/java/br/gov/agu/samir/new_samir_back/NewSamirBackApplication.java
@@ -0,0 +1,13 @@
+package br.gov.agu.samir.new_samir_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSamirBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSamirBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/agu/samir/new_samir_back/NewSamirBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.agu.samir.new_samir_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSamirBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
