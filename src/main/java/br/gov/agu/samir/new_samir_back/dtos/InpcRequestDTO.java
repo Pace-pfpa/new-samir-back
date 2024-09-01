cfab80c8a00ccad94dc16866eb435f8075144f39
Danilo Chaves <danchavesds@gmail.com>
2024-08-31 22:49:00 -0300

    feat: adicionando inpc dtos

---
+++ src/main/java/br/gov/agu/samir/new_samir_back/dtos/InpcRequestDTO.java
@@ -0,0 +1,20 @@
+package br.gov.agu.samir.new_samir_back.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InpcRequestDTO {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate data;
+
+    private BigDecimal valor;
+}
