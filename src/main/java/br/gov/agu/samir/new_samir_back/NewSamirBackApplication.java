15c35befd915b68db1739c6c591f8373edfba488
Danilo Chaves <danchavesds@gmail.com>
2024-09-06 13:06:09 -0300

    feat: atualizando selic automaticamnete

--- src/main/java/br/gov/agu/samir/new_samir_back/NewSamirBackApplication.java
+++ src/main/java/br/gov/agu/samir/new_samir_back/NewSamirBackApplication.java
@@ -2,8 +2,10 @@ package br.gov.agu.samir.new_samir_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NewSamirBackApplication {
 
 	public static void main(String[] args) {
