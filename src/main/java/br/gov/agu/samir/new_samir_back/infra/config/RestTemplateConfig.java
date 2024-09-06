6c69edad0a4d22143c78fd2347e8808195015e33
Danilo Chaves <danchavesds@gmail.com>
2024-09-06 11:52:01 -0300

    feat: Agendamento para salvar selic automaticamente no banco

---
+++ src/main/java/br/gov/agu/samir/new_samir_back/infra/config/RestTemplateConfig.java
@@ -0,0 +1,4 @@
+package br.gov.agu.samir.new_samir_back.infra.config;
+
+public class RestTemplateConfig {
+}
---
+++ src/main/java/br/gov/agu/samir/new_samir_back/service/AgendamentoSelicService.java
@@ -0,0 +1,4 @@
+package br.gov.agu.samir.new_samir_back.service;
+
+public class AgendamentoSelicService {
+}
