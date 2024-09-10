2c3eba6d85322df473e65d52a766da89bc116ded
Danilo Chaves <danchavesds@gmail.com>
2024-09-09 21:34:05 -0300

    feat: crud completo beneficio inacumulavel

--- src/main/java/br/gov/agu/samir/new_samir_back/mapper/BeneficioInacumulavelMapper.java
+++ src/main/java/br/gov/agu/samir/new_samir_back/mapper/BeneficioInacumulavelMapper.java
@@ -1,2 +1,20 @@
-package br.gov.agu.samir.new_samir_back.mapper;public interface BeneficioInacumulavelMapper {
+package br.gov.agu.samir.new_samir_back.mapper;
+
+import br.gov.agu.samir.new_samir_back.dtos.BeneficioInacumulavelRequestDTO;
+import br.gov.agu.samir.new_samir_back.dtos.BeneficioInacumulavelResponseDTO;
+
+import br.gov.agu.samir.new_samir_back.models.BeneficioInacumulavelModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface BeneficioInacumulavelMapper {
+
+    @Mapping(target = "id", ignore = true)
+    BeneficioInacumulavelModel toModel(BeneficioInacumulavelRequestDTO beneficioInacumulavelRequestDTO);
+
+    BeneficioInacumulavelResponseDTO toResponseDTO(BeneficioInacumulavelModel beneficioInacumulavelModel);
 }
--- src/main/java/br/gov/agu/samir/new_samir_back/service/BeneficioInacumulavelService.java
+++ src/main/java/br/gov/agu/samir/new_samir_back/service/BeneficioInacumulavelService.java
@@ -1,2 +1,48 @@
-package br.gov.agu.samir.new_samir_back.service;public class BeneficioInacumulavelService {
+package br.gov.agu.samir.new_samir_back.service;
+
+import br.gov.agu.samir.new_samir_back.dtos.BeneficioInacumulavelRequestDTO;
+import br.gov.agu.samir.new_samir_back.dtos.BeneficioInacumulavelResponseDTO;
+import br.gov.agu.samir.new_samir_back.dtos.BeneficioRequestDTO;
+import br.gov.agu.samir.new_samir_back.exceptions.ResourceNotFoundException;
+import br.gov.agu.samir.new_samir_back.mapper.BeneficioInacumulavelMapper;
+import br.gov.agu.samir.new_samir_back.models.BeneficioInacumulavelModel;
+import br.gov.agu.samir.new_samir_back.repository.BeneficioInacumulavelRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BeneficioInacumulavelService {
+
+    private final BeneficioInacumulavelMapper mapper;
+
+    private final BeneficioInacumulavelRepository repository;
+
+    public BeneficioInacumulavelService(BeneficioInacumulavelMapper mapper, BeneficioInacumulavelRepository repository) {
+        this.mapper = mapper;
+        this.repository = repository;
+    }
+
+    public BeneficioInacumulavelResponseDTO salvarBeneficioInacumulavel(BeneficioInacumulavelRequestDTO requestDTO) {
+        BeneficioInacumulavelModel model = mapper.toModel(requestDTO);
+        repository.save(model);
+        return mapper.toResponseDTO(model);
+    }
+
+    public BeneficioInacumulavelResponseDTO buscarBeneficioInacumulavelPorId(Long id) {
+        BeneficioInacumulavelModel model = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Beneficio não encontrado"));
+        return mapper.toResponseDTO(model);
+    }
+
+
+    public BeneficioInacumulavelResponseDTO atualizarBeneficioInacumulavel(Long id, BeneficioInacumulavelRequestDTO requestDTO) {
+        BeneficioInacumulavelModel model = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Beneficio não encontrado"));
+        BeneficioInacumulavelModel updatedModel = BeneficioInacumulavelModel.builder()
+                .id(model.getId())
+                .nome(requestDTO.getNome() != null ? requestDTO.getNome() : model.getNome())
+                .build();
+        return mapper.toResponseDTO(updatedModel);
+    }
+
+    public void deletarBeneficioInacumulavel(Long id) {
+        repository.deleteById(id);
+    }
 }
