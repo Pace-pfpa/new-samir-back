348afdaebacb5cbbb51d5accf3ccefe7fd5f5075
Danilo Chaves <danchavesds@gmail.com>
2024-09-09 21:16:23 -0300

    feat: crud completo Beneficio

--- src/main/java/br/gov/agu/samir/new_samir_back/service/BeneficioService.java
+++ src/main/java/br/gov/agu/samir/new_samir_back/service/BeneficioService.java
@@ -2,6 +2,7 @@ package br.gov.agu.samir.new_samir_back.service;
 
 import br.gov.agu.samir.new_samir_back.dtos.BeneficioRequestDTO;
 import br.gov.agu.samir.new_samir_back.dtos.BeneficioResponseDTO;
+import br.gov.agu.samir.new_samir_back.exceptions.ResourceNotFoundException;
 import br.gov.agu.samir.new_samir_back.mapper.BeneficioMapper;
 import br.gov.agu.samir.new_samir_back.models.BeneficioInacumulavelModel;
 import br.gov.agu.samir.new_samir_back.models.BeneficioModel;
@@ -33,8 +34,40 @@ public class BeneficioService {
         model.setBeneficiosInacumulaveis(beneficioInacumulavelModels);
         beneficioRepository.save(model);
 
+        BeneficioResponseDTO responseDTO = mapper.toResponseDTO(model);
+
+        responseDTO.setBeneficiosInacumulaveis(mapper.toListResponseInacumulavelDTO(beneficioInacumulavelModels));
+
+        return responseDTO;
+
+    }
+
+    public BeneficioResponseDTO buscarBeneficioPorId(Long id){
+        BeneficioModel model = beneficioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Beneficio não encontrado"));
+        BeneficioResponseDTO responseDTO = mapper.toResponseDTO(model);
+        responseDTO.setBeneficiosInacumulaveis(mapper.toListResponseInacumulavelDTO(model.getBeneficiosInacumulaveis()));
+        return responseDTO;
     }
 
 
+    public BeneficioResponseDTO atualizarBeneficio(Long id, BeneficioRequestDTO requestDTO) {
+        BeneficioModel model = beneficioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Beneficio não encontrado"));
 
+
+        List<BeneficioInacumulavelModel> beneficioInacumulavelModels = inacumulavelRepository.findAllById(requestDTO.getBeneficiosInacumulaveisIds());
+
+        BeneficioResponseDTO responseDTO = BeneficioResponseDTO.builder().
+                id(model.getId())
+                .diff(requestDTO.getDiff() != null ? requestDTO.getDiff() : model.getDiff())
+                .decimoTerceiro(requestDTO.getDecimoTerceiro() != null ? requestDTO.getDecimoTerceiro() : model.getDecimoTerceiro())
+                .nome(requestDTO.getNome() != null ? requestDTO.getNome() : model.getNome())
+                .beneficiosInacumulaveis(requestDTO.getBeneficiosInacumulaveisIds() != null ? mapper.toListResponseInacumulavelDTO(inacumulavelRepository.findAllById(requestDTO.getBeneficiosInacumulaveisIds())) : mapper.toListResponseInacumulavelDTO(model.getBeneficiosInacumulaveis()))
+                .build();
+
+        return responseDTO;
+    }
+
+    public void deletarBeneficio(Long id) {
+        beneficioRepository.deleteById(id);
+    }
 }
