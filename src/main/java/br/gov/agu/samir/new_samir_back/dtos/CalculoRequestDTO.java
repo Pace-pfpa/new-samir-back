c6ed73afadc736ed339494c71266df4cec880142
Danilo Chaves <danchavesds@gmail.com>
2024-10-05 12:27:37 -0300

    feat: adicionando rmi proporcional

--- src/main/java/br/gov/agu/samir/new_samir_back/dtos/CalculoRequestDTO.java
+++ src/main/java/br/gov/agu/samir/new_samir_back/dtos/CalculoRequestDTO.java
@@ -19,6 +19,7 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +39,8 @@ public class CalculoRequestDTO {
     private LocalDate dataFim;
 
     private BigDecimal rmi;
+
+    private int porcentagemRmi;
 
     @JsonFormat(pattern = "dd/MM/yyyy", shape = JsonFormat.Shape.STRING)
     private LocalDate dataIncioJuros; //Data citação -> vai ter juros quando a citação for anterior a 12/2021
@@ -59,17 +62,15 @@ public class CalculoRequestDTO {
     private BeneficioRepository beneficioRepository;
 
 
-    public List<BeneficioAcumuladoRequestDTO> getBeneficioValidos() {
-        List<BeneficioAcumuladoRequestDTO> beneficiosAcumulados = this.getBeneficioAcumulados();
-        List<BeneficioInacumulavelModel> beneficiosInacumulaveis = beneficioRepository.findByNome(beneficio.getDescricao()).getBeneficiosInacumulaveis();
-
-        return beneficiosAcumulados.stream()
-                .filter(beneficioAcumulado -> verificaSeBeneficioEhValido(beneficiosInacumulaveis, beneficioAcumulado.getBeneficioAcumulado().getDescricao()))
-                .toList();
+    public BigDecimal getRmi(){
+        return this.rmi != null ? rmi.multiply(BigDecimal.valueOf(porcentagemRmi)).divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP) : getRmi();
     }
 
+    public List<BeneficioAcumuladoRequestDTO> getBeneficioInacumulaveisParaCalculo() {
+        List<BeneficioAcumuladoRequestDTO> beneficiosAcumulados = this.beneficioAcumulados;
+        List<BeneficioInacumulavelModel> beneficiosInacumulaveis = beneficioRepository.findByNome(this.beneficio.getDescricao()).getBeneficiosInacumulaveis();
 
-    private boolean verificaSeBeneficioEhValido(List<BeneficioInacumulavelModel> listInacumalaveis, String nomeBeneficio){
-        return listInacumalaveis.stream().map(BeneficioInacumulavelModel::getNome).toList().contains(nomeBeneficio);
+        return beneficiosAcumulados.stream()
+                .filter(beneficioAcumulado -> beneficiosInacumulaveis.contains(beneficioAcumulado.getBeneficioAcumulado())).toList();
     }
 }
