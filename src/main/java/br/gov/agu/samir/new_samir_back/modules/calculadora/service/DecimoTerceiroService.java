7ff64570ebe487af441f03cf95e2d5cfbf5681bc
Danilo Chaves <danchavesds@gmail.com>
2025-01-24 13:46:50 -0300

    feat: impl do décimo terceiro ao final do calculo

--- src/main/java/br/gov/agu/samir/new_samir_back/modules/calculadora/service/DecimoTerceiroService.java
+++ src/main/java/br/gov/agu/samir/new_samir_back/modules/calculadora/service/DecimoTerceiroService.java
@@ -1,8 +1,6 @@
 package br.gov.agu.samir.new_samir_back.modules.calculadora.service;
 
 
-
-
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -12,27 +10,37 @@ import java.time.LocalDate;
 @Service
 public class DecimoTerceiroService {
 
-//TODO: Refatorar
-public BigDecimal calcularDecimoTerceiro(String dataDecimoTerceiro,LocalDate dataInicio, LocalDate fimCalculo, BigDecimal rmi) {
+    //TODO: Refatorar
+    public BigDecimal calcularDecimoTerceiro(String dataDecimoTerceiro, LocalDate dataInicio, LocalDate fimCalculo, BigDecimal rmi) {
 
-  if (isDecimoTerceiroParcial(dataDecimoTerceiro, dataInicio, dataInicio)) {
-      int mesesTrabalhados = calcularMesesTrabalhados(dataDecimoTerceiro, dataInicio, dataInicio);
-      return rmi.multiply(BigDecimal.valueOf(mesesTrabalhados)).divide(BigDecimal.valueOf(12), 2, RoundingMode.HALF_UP);
-  }
-  return rmi;
-}
+        if (isDecimoTerceiroParcial(dataDecimoTerceiro, dataInicio, fimCalculo)) {
+            int mesesTrabalhados = calcularMesesTrabalhados(dataDecimoTerceiro, dataInicio, fimCalculo);
+            return rmi.multiply(BigDecimal.valueOf(mesesTrabalhados)).divide(BigDecimal.valueOf(12), 2, RoundingMode.HALF_UP);
+        }
+        return rmi;
+    }
 
 
-    private int calcularMesesTrabalhados(String decimoTerceiro ,LocalDate dataInicio, LocalDate fimCalculo) {
+    private int calcularMesesTrabalhados(String decimoTerceiro, LocalDate dataInicio, LocalDate fimCalculo) {
 
         int anoDecimoTerceiro = Integer.parseInt(decimoTerceiro.split("/")[2]);
+        if (anoDecimoTerceiro == dataInicio.getYear()) {
 
-        int mesesTrabalhados = 12 - dataInicio.getMonthValue();
+            if (dataInicio.getDayOfMonth() < 17) {
+                return 13 - dataInicio.getMonthValue();
+            } else {
+                return 12 - dataInicio.getMonthValue();
+            }
+        } else {
 
-        if (dataInicio.getDayOfMonth() < 17) {
-            mesesTrabalhados = 13 - dataInicio.getMonthValue();
+            if (fimCalculo.getDayOfMonth() > 17) {
+                return fimCalculo.getMonthValue();
+            } else {
+                return fimCalculo.getMonthValue() - 1;
+            }
+
         }
-        return mesesTrabalhados;
+
     }
 
     private boolean isDecimoTerceiroParcial(String decimoTerceiro, LocalDate dataInicio, LocalDate dataFim) {
