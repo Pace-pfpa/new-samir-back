5e4d86ae04e74d683c3812cf96b9d19fb9115dfa
Danilo Chaves <danchavesds@gmail.com>
2025-01-27 11:42:26 -0300

    refact: refatorando agendamento selic

--- src/main/java/br/gov/agu/samir/new_samir_back/modules/selic/service/AgendamentoSelicService.java
+++ src/main/java/br/gov/agu/samir/new_samir_back/modules/selic/service/AgendamentoSelicService.java
@@ -2,7 +2,6 @@ package br.gov.agu.samir.new_samir_back.modules.selic.service;
 
 import br.gov.agu.samir.new_samir_back.modules.selic.dto.SelicRequestDTO;
 import br.gov.agu.samir.new_samir_back.modules.selic.mapper.SelicMapper;
-import br.gov.agu.samir.new_samir_back.modules.selic.model.SelicModel;
 import br.gov.agu.samir.new_samir_back.modules.selic.repository.SelicRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,8 +12,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
+
 
 @Service
 @Slf4j
@@ -31,26 +29,23 @@ public class AgendamentoSelicService {
         LocalDate dataAtual = LocalDate.now();
         String dataAtualFormatada = dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
-        try{
-            SelicRequestDTO dto = webClient.get()
-                    .uri(uriBuilder -> uriBuilder
-                            .queryParam("formato", "json")
-                            .queryParam("dataInicial", dataAtualFormatada)
-                            .queryParam("dataFinal", dataAtualFormatada)
-                            .build())
-                    .retrieve()
-                    .bodyToMono(SelicRequestDTO[].class)
-                    .block()[0];
 
-            if (!selicRepository.existsByData(dto.getData())) {
-                selicRepository.save(selicMapper.mapToModel(dto));
-                log.info("Salvando Selic no banco de dados");
-            } else {
-                log.info("Selic já existe no banco de dados");
-            }
+        SelicRequestDTO dto = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("formato", "json")
+                        .queryParam("dataInicial", dataAtualFormatada)
+                        .queryParam("dataFinal", dataAtualFormatada)
+                        .build())
+                .retrieve()
+                .bodyToFlux(SelicRequestDTO.class)
+                .blockFirst();
 
-        } catch (RuntimeException e) {
-            log.info("ERRO AO SALVAR SELIC");
+        assert dto != null;
+        if (!selicRepository.existsByData(dto.getData())) {
+            selicRepository.save(selicMapper.mapToModel(dto));
+            log.info("Salvando Selic no banco de dados");
+        } else {
+            log.info("Selic já existe no banco de dados");
         }
 
     }
